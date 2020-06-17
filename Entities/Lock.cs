34934464046dad346c5dcd0ab055dc882832abd7
Jesse Purice <jessepurice@gamil.com>
2020-06-17 16:03:50 -0700

    Added Lock class

---
+++ Entities/Lock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solun.Entities
+{
+	class Lock : Entity
+	{
+		Entity unlockEntity;
+
+		bool isLocked;
+
+		public bool IsLocked => isLocked;
+
+		public Lock(string name, string description, Entity unlockEntity, bool isLocked)
+		{
+			this.name = name;
+			this.description = description;
+
+			this.unlockEntity = unlockEntity;
+			this.isLocked = isLocked;
+		}
+
+		public override void Interact(Entity entity)
+		{
+			if(entity == unlockEntity) isLocked = !isLocked;
+		}
+	}
+}
